ac050c1f5f96f41370aa985d0bbd2e8155d4d68d
ldselvera <ldselvera@gmail.com>
2021-05-29 15:49:12 -0500

    recover model

--- deploy.py
+++ deploy.py
@@ -11,20 +11,19 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-     <h1> hello </h1>
- #   return render_template('index.html')
+   return render_template('index.html')
 
-# @app.route('/classification', methods=['POST', 'GET'])
-# def result():
-#   if request.method == 'POST':
-#     question = request.form['text']
+@app.route('/classification', methods=['POST', 'GET'])
+def result():
+  if request.method == 'POST':
+    question = request.form['text']
   
-#   if question=='':
-#     answer="Please provide a question."
-#   else:
-#     answer='Sincere question.'
-#     #answer = prediction(question, model, tokenizer)
-#   return render_template("classification.html", result= answer)
+  if question=='':
+    answer="Please provide a question."
+  else:
+    answer='Sincere question.'
+    #answer = prediction(question, model, tokenizer)
+  return render_template("classification.html", result= answer)
 
-# if __name__ == '__main__':
-#   app.run()
+if __name__ == '__main__':
+  app.run()
